1193b78b718f32f88d69e20437b87f9f50486c28
last order <2495713984@qq.com>
2024-04-04 16:43:11 +0900

    电影列表新增一些内容

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,11 @@ import { AntdRegistry } from '@ant-design/nextjs-registry'
 import React from 'react'
 
 import type { MenuProps } from 'antd'
-import { Menu, Breadcrumb, Layout, theme } from 'antd'
+import { Menu, Breadcrumb, Layout, theme,Dropdown, Space, Avatar } from 'antd'
+import { DownOutlined } from '@ant-design/icons'
 import { usePathname } from 'next/navigation'
 import { menu } from '../config/menu'
 import '../assets/css/normalize.scss'
-
-
 
 export default function RootLayout({
   children,
@@ -24,16 +23,39 @@ export default function RootLayout({
 
  const onClick: MenuProps['onClick'] = (e) => {
  };
- 
+
+ const items: MenuProps['items'] = [
+    {
+      label: (
+        <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
+          1st menu item
+        </a>
+      ),
+      key: '0',
+    },
+    {
+      label: (
+        <a target="_blank" rel="noopener noreferrer" href="https://www.aliyun.com">
+          2nd menu item
+        </a>
+      ),
+      key: '1',
+    }
+  ];
+  const url = 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg';
+
   return (
     <html lang="en">
       <body>
         <AntdRegistry>
           <Layout style={{
+            width: '100vw',
             height: '100vh'
           }}>
-            <Header style={{ display: 'flex', alignItems: 'center' }}>
-              <div className="demo-logo" />
+            <Header style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}>
+              <Dropdown menu={{ items }} placement='bottomCenter'>
+                <Avatar src={url} />
+              </Dropdown>
             </Header>
             <Layout>
               <Sider width={200} style={{ background: colorBgContainer }}>
--- src/app/movie/page.tsx
+++ src/app/movie/page.tsx
@@ -1,14 +1,24 @@
 'use client'
 import React from 'react';
-import { Table } from 'antd';
+import { Table, Button, Space, Row, Col, Image, Tag } from 'antd';
 import { useState, useEffect } from 'react';
 import type { TableColumnsType } from 'antd';
+import movie from '../../assets/image/conan-movie.png'
+import { status } from '../../config/index'
 
 export default function Movie () {
   const [data, setData] = useState([
     {
-      name: 'すずめの戸締り',
+      name: '劇場版『名探偵コナン 100万ドルの五稜星（みちしるべ）』',
       level: 'G',
+      time: 113,
+      watchCount: 1000,
+      commentCount: 100
+    },
+    {
+      name: '劇場版『名探偵コナン 100万ドルの五稜星（みちしるべ）』',
+      level: 'G',
+      time: 113,
       watchCount: 1000,
       commentCount: 100
     }
@@ -17,7 +27,33 @@ export default function Movie () {
     {
       title: 'タイトル',
       dataIndex: 'name',
-      width: 150,
+      width: 350,
+      render (_, row) {
+        return (
+          <Space align="start">
+            <Image width={100} src={movie.src} alt="poster"></Image>
+            <Space direction="vertical">
+              <span>{row.name}</span>
+              <section>
+                {
+                  ['IMAX', 'DOLBY cinema', '2D', 'DOLBY ATOMS'].map(item => {
+                    return <Tag key={item} style={{
+                      marginBottom: '10px'
+                    }}>{item}</Tag>
+                  })
+                }
+              </section>
+            </Space>
+          </Space>
+        )
+      }
+    },
+    {
+      title: 'タイム',
+      dataIndex: 'time',
+      render(text: number) {
+        return <span>{text}分</span>
+      }
     },
     {
       title: 'レベル',
@@ -38,7 +74,28 @@ export default function Movie () {
     {
       title: '上映終了時期',
       dataIndex: '',
-    }
+    },
+    {
+      title: '上映ステータス',
+      dataIndex: '',
+      render (text: number) {
+        return <span>{status[1]}</span>
+      }
+    },
+    {
+      title: '操作',
+      key: 'operation',
+      fixed: 'right',
+      // width: 100,
+      render: () => {
+        return (
+          <Space>
+            <Button type="primary">編集</Button>
+            <Button type="primary" danger>削除</Button>
+          </Space>
+        )
+      },
+    },
   ]
 
   return (
