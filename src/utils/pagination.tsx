e2ca418bcb8b6d96b17b24e655dd8ac5e2e78231
last order <2495713984@qq.com>
2024-05-30 20:09:07 +0900

    新增回复列表

--- src/utils/pagination.tsx
+++ src/utils/pagination.tsx
@@ -1,5 +1,5 @@
 export function showTotal(total: number, range: number[]) {
   const [start, end] = range
 
-  return `总共 ${total} 条, 当前${start}~ ${end}条`
+  return `总共 ${total} 条, 当前${start} ~ ${end}条`
 }
